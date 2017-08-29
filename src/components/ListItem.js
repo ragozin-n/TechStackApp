73628ddc3149fbd1773dd6be03717aaf1ddac102
Ragozin Nikita <lastjanuary@yandex.ru>
2017-08-29 19:31:24 +0500

    Animations + some little improvements

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { 
+    Text, 
+    TouchableWithoutFeedback, 
+    View,
+    LayoutAnimation 
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from "./common";
 import * as actions from '../actions';
 
 class ListItem extends Component {
+    
+    componentWillUpdate() {
+        LayoutAnimation.easeInEaseOut();
+    }
+    
     renderDesc() {
         const { item, expanded } = this.props;
 
         if (expanded) {
             return (
-                <Text>{item.description}</Text>
+                <CardSection>
+                    <Text style={{flex: 1}}>
+                        {item.description}
+                    </Text>
+                </CardSection>
             );
         }
     }
