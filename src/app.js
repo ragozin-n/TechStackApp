91ec06241dc67e3318b9d53aeaccd642d7f696f7
Ragozin Nikita <lastjanuary@yandex.ru>
2017-08-29 16:44:40 +0500

    Redux init

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
- import React from 'react';
- import { View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+ 
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+            <View />
+        </Provider>
+    );
+};
 
- const App = () => {
-     return ( 
-         <View />
-     );
- };
-
- export default App;
+export default App;
